4c0f6d0a6f5fb009d850633fb38a469df321fe0e
N1ebieski <kontakt@intelekt.net.pl>
2025-10-30 21:55:29 +0100

    rector fixes

--- tests/Feature/AbstractTestCase.php
+++ tests/Feature/AbstractTestCase.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace N1ebieski\KSEFClient\Tests\Feature;
 
 use N1ebieski\KSEFClient\ClientBuilder;
--- tests/Unit/AbstractTestCase.php
+++ tests/Unit/AbstractTestCase.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace N1ebieski\KSEFClient\Tests\Unit;
 
 use Mockery;
